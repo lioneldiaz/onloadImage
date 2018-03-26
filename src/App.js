28e07817408792449ba4f37356c8af4d6f971587
DevelopHack <lionel2688@gmail.com>
2018-03-26 14:11:01 -0500

    feat: a new feature
    Show image in the window and crop it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
+import Modal from 'react-modal'
 import photo from './image.jpg'
+import FaTimesCircle from 'react-icons/lib/fa/times-circle'
+import './App.css'
 
 class App extends Component {
   /**
@@ -10,7 +13,14 @@ class App extends Component {
     this.state = {
       url: photo,
       angle: 180,
+      show: false,
+      save: false
     }
+  }
+  showWindow = () => this.setState(() => ({show: true}))
+  closeWindow = () => {
+    this.setState(() => ({show: false}))
+    
   }
   /**
    * @description Draw a image in the canva
@@ -81,7 +91,8 @@ class App extends Component {
           }        
     }, true)
   }
-  componentDidMount () {
+  
+  afterOpenModal = () => {
     this.draw()
     this.drawRegion()
   }
@@ -91,48 +102,85 @@ class App extends Component {
   rotate = () => {
     const ctx = this.canvas.getContext('2d')    
     ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    
     ctx.translate(this.canvas.width/2,this.canvas.height/2)
     ctx.rotate(this.state.angle * Math.PI/180)
     ctx.translate(-this.canvas.width/2,-this.canvas.height/2)
+    
     ctx.drawImage(this.img, 0, 0, this.img.naturalWidth, this.img.naturalHeight, 0, 0, this.canvas.width, this.canvas.height)
   }
-  getImage = () => {
+  getImage = () => {    
     /**
      * @description Take the coordinates of the image
      */
-    const v = document.getElementById('mark')  
+    const v = document.getElementById('mark')
     let sx = parseInt(v.style.left.slice(0,-2))
     let sy = parseInt(v.style.top.slice(0,-2))
     /**
      * @description To caught the image in the canava
      */
     const ctx = this.canvas.getContext('2d')
-    const imgData = ctx.getImageData(sx,sy,100,100)
-    
-    const child = document.getElementById('new')
-    /**
-     * @description Remove the node inside of the element child if it exist
-     */
-    if (child.lastChild !== null)
-      child.removeChild(document.getElementById('newImage'))
-
+    const imgData = ctx.getImageData(sx,sy,100,100)        
     /**
      * @description Create a new canva to insert in the child
      */
-    const canva = document.createElement('canvas')
-    canva.setAttribute('id','newImage')
-    const context = canva.getContext('2d')
-    context.putImageData(imgData, 0, 0)      
-    child.appendChild(canva)  
+    this.newCanva = document.createElement('canvas')
+    this.newCanva.setAttribute('id','newImage')
+    const context = this.newCanva.getContext('2d')
+    context.putImageData(imgData, 0, 0)
+    this.setState(() => ({save: true})) 
+    this.closeWindow()
   }
+  /**
+   *@description Invoke immediately after updating occurs 
+   */
+  componentDidUpdate () {
+    if (!this.state.show && this.state.save) {
+      const child = document.getElementById('new')
+      /**
+       * @description Remove the node inside of the element child if it exist
+       */
+      if (child.lastChild !== null)
+        child.removeChild(document.getElementById('newImage'))
+      child.appendChild(this.newCanva)
+
+      this.setState({save: false})
+    }    
+  }  
   render() {
+    if (this.state.show) {
+      return (
+        <Modal
+          isOpen={this.state.show}
+          onRequestClose={this.closeWindow}
+          onAfterOpen={this.afterOpenModal}
+          style={{
+            top: '50%',
+            left: '50%',
+            right: '50%',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)'
+          }}
+          ariaHideApp={false}
+        >
+          <div>
+            <div style={{float:'left'}}>Crop your picture</div>
+            <button className='icon-btn' style={{float:'right'}} onClick={this.closeWindow}>
+              <FaTimesCircle size={30}/>
+            </button>
+          </div>
+          <div id="box"/>        
+            <div style={{marginLeft:100}}>
+              <button onClick={this.getImage}>Save</button>
+              <button onClick={this.rotate}>Rotate</button>
+          </div>
+        </Modal>  
+      )
+    }
     return (
       <div>
-        <div id="box"/>        
-        <div style={{marginLeft:100}}>
-          <button onClick={this.getImage}>Save</button>
-          <button onClick={this.rotate}>Rotate</button>
-        </div>
+        <button onClick={this.showWindow}>Show</button>        
         <div id="new" style={{marginLeft:100, marginTop:10}}></div>       
       </div>
     )
